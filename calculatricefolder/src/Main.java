ab35ff669424806b28d6bad16c6ed731dbefcbf7
wee7zzy <yassaniq@gmail.com>
2024-12-12 18:37:01 +0100

    CREATION DE LA FONCTION MENU AU JE VAIS AFFICHER A L'UTILISATEUR LES DIFFERENTS OPPERATIONS POUR CHOISIR

---
+++ calculatricefolder/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+
+    public static void MENU(){
+        System.out.print("-------------MENU----------------\n");
+    }
+
+    public static void main(String[] args) {
+            MENU();
+    }
+}
+
+
+
