ceb43bfd6d0b9198b8d1309acbadc77680a7cef4
wee7zzy <yassaniq@gmail.com>
2024-12-12 19:08:58 +0100

    CREATION DE LA FONCTION ADDITION ET MODIFIER LE MENU

--- calculatricefolder/src/Main.java
+++ calculatricefolder/src/Main.java
@@ -3,11 +3,31 @@ import java.util.Scanner;
 public class Main{
 
     public static void MENU(){
-        System.out.print("-------------MENU----------------\n");
+        System.out.print("-------------MENU----------------\n"+
+                        "1-ADDITION\n"+
+                        "--------ENTRER VOTRE  CHOIX :");
+        choix=scanner.nextInt();
+    }
+    
+    public static void addition() {
+        System.out.print("entrer a :");
+        a = scanner.nextDouble();
+        System.out.print("entrer b :");
+        b = scanner.nextDouble();
+        System.out.println("la somme a+b =" + (a+b));
     }
 
     public static void main(String[] args) {
+        do {
             MENU();
+            switch (choix) {
+                case 1:
+                    addition();
+                    break;
+
+            }
+        } while (choix !=1);
+        scanner.close();
     }
 }
 
