581c5a0d12bd42f1ea6917625a12630bf12ea59f
wee7zzy <yassaniq@gmail.com>
2024-12-13 09:29:32 +0100

    CREATIONE DE LA FONCTION SOUSTRACTION ET AJOUTER LE CHOIX QUITTER POUR SORTIR DE LA BOUCLE

--- calculatricefolder/src/Main.java
+++ calculatricefolder/src/Main.java
@@ -1,10 +1,16 @@
 import java.util.Scanner;
 
 public class Main{
+    
+    static int choix;
+    static double a,b;
+    static SCANNER scanner=new Scanner(System.in);
 
     public static void MENU(){
         System.out.print("-------------MENU----------------\n"+
                         "1-ADDITION\n"+
+                        "2-SOUSTRACTION\n"+
+                        "3-QUITTER\n"+
                         "--------ENTRER VOTRE  CHOIX :");
         choix=scanner.nextInt();
     }
@@ -16,6 +22,14 @@ public class Main{
         b = scanner.nextDouble();
         System.out.println("la somme a+b =" + (a+b));
     }
+   
+    public static void soustraction() {
+        System.out.print("entrer a :");
+        a = scanner.nextDouble();
+        System.out.print("entrer b :");
+        b = scanner.nextDouble();
+        System.out.println("la soustraction a-b =" + (a-b));
+    }
 
     public static void main(String[] args) {
         do {
@@ -24,9 +38,15 @@ public class Main{
                 case 1:
                     addition();
                     break;
+                case 2:
+                    soustraction();
+                    break;
+                case 3:
+                    System.out.println("MERCI D'UTILISER LA CALCULATRICE A BIENTOT");
+                    break;
 
             }
-        } while (choix !=1);
+        } while (choix !=3);
         scanner.close();
     }
 }
